330fd6e4bd5eb91d4a741ff73f3762f08342ddf2
Kenneth <kennyn510@yahoo.com>
2016-12-16 17:02:24 -0800

    Update manipulate.sh

--- manipulate.sh
+++ manipulate.sh
@@ -10,20 +10,20 @@ read -p "> " choice
 
 case $choice in
 	1) read -p "Enter path to file: " f_path
-	awk '!(count[$0]++)' "$f_path" > new-${f_path}
-	echo "Duplicates have been removed, view new file at $PWD/new-$f_path"
+	awk '!(count[$0]++)' "$f_path" > ${f_path}-new
+	[ $? -eq 0 ] && { echo "Duplicates have been removed, view new file at $PWD/${f_path}-new"; exit 0; }
 	;;
 	2) read -p "Enter path to file: " f_path
-	awk '{print length, $0}' "$f_path" | sort -n | cut -d " " -f2- > new-${f_path}
-	echo "File has been sorted by length at $PWD/new-$f_path"
+	awk '{print length, $0}' "$f_path" | sort -n | cut -d " " -f2- > ${f_path}-new
+	[ $? -eq 0 ] && { echo "File has been sorted by length at $PWD/${f_path}-new"; exit 0; }
 	;;
 	3) read -p "Enter path to file: " f_path
-	sort "$f_path" | uniq > new-${f_path}
-	echo "File has been sorted by alphabetical order at $PWD/new-$f_path"
+	sort "$f_path" | uniq > ${f_path}-new
+	[ $? -eq 0 ] && { echo "File has been sorted by alphabetical order at $PWD/${f_path}-new"; exit 0; }
 	;;
 	4) read -p "Enter path to file: " f_path
-	egrep -v "^[[:space:]]*$" "$f_path" > new-${f_path}
-	echo "File has been removed of all blank lines at $PWD/new-$f_path"
+	egrep -v "^[[:space:]]*$" "$f_path" > ${f_path}-new
+	[ $? -eq 0 ] && { echo "File has been removed of all blank lines at $PWD/${f_path}-new"; exit 0; }
 	;;
 	q) echo "Peace"
 	;;
